0635885c8a9e7a779064e22c726e82f4e5bc568a
SaraSanchez <sara_sanchez82221@elpoli.edu.co>
2025-10-23 21:41:04 -0500

    Add component footer

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Topbar } from './topbar/topbar';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Topbar],
+  imports: [RouterOutlet, Topbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
