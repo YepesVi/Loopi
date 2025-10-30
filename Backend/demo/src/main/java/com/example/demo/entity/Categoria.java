dd034d2a4356435713deb0a83daf114fdfa82630
YepesVi <andresyepepesvi1@gmail.com>
2025-10-30 10:15:58 -0500

    Solucion StackOverflow

--- Backend/demo/src/main/java/com/example/demo/entity/Categoria.java
+++ Backend/demo/src/main/java/com/example/demo/entity/Categoria.java
@@ -3,6 +3,9 @@ package com.example.demo.entity;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -40,10 +43,12 @@ public class Categoria {
     // Relación Muchos a Uno con el Padre (la misma entidad)
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_id")
+    @JsonBackReference
     private Categoria parent; // El nodo padre
 
     // Relación Uno a Muchos con los Hijos (la misma entidad)
     @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
     private Set<Categoria> hijos = new HashSet<>(); // La lista de nodos hijos
   
     
---
+++ Backend/demo/src/main/java/com/example/demo/exception/GlobalExtensionHandler.java
@@ -0,0 +1,27 @@
+package com.example.demo.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+// Maneja las excepciones globales de validación y otras excepciones personalizadas para devolver respuestas adecuadas.
+@RestControllerAdvice
+public class GlobalExtensionHandler {
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationErrors(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+
+}
