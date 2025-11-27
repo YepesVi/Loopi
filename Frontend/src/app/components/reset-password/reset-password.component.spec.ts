d08c45c5d89984b2bbcb6c1252518a66ecece08c
Derinson <Derinson_valencia23201@elpoli.edu.co>
2025-11-27 14:01:54 -0500

    github action

--- Frontend/src/app/components/reset-password/reset-password.component.spec.ts
+++ Frontend/src/app/components/reset-password/reset-password.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ResetPasswordComponent } from './reset-password.component';
-import { AuthService } from '../services/auth.service';
+import { AuthService } from '../../services/auth.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { of, throwError } from 'rxjs';
--- Frontend/src/app/services/carrito/carrito-service.spec.ts
+++ Frontend/src/app/services/carrito/carrito-service.spec.ts
@@ -1,16 +1,56 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { CarritoService } from './carrito-service';
 
 describe('CarritoService', () => {
   let service: CarritoService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [CarritoService]
+    });
+
     service = TestBed.inject(CarritoService);
+    httpMock = TestBed.inject(HttpTestingController);
+
+    // ✅ Simular localStorage
+    spyOn(localStorage, 'getItem').and.callFake((key: string) => {
+      if (key === 'cedula') return '123';
+      return null;
+    });
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+ 
+
+ it('debería agregar producto con cedula en body', () => {
+  const mockResponse = { mensaje: 'Producto agregado' };
+
+  service.agregarProducto(12345).subscribe(res => {
+    expect(res.mensaje).toBe('Producto agregado');
+  });
+
+  const req = httpMock.expectOne('http://localhost:8081/api/carrinho/agregar/12345');
+  expect(req.request.method).toBe('POST');
+  expect(req.request.body).toEqual({ cedula: '123' });
+  req.flush(mockResponse);
+});
+
+
+  it('debería eliminar producto por itemId', () => {
+    const mockResponse = { mensaje: 'Producto eliminado' };
+
+    service.eliminarProducto(7).subscribe(res => {
+      expect(res.mensaje).toBe('Producto eliminado');
+    });
+
+    const req = httpMock.expectOne('http://localhost:8081/api/carrito/item/7');
+    expect(req.request.method).toBe('DELETE');
+    req.flush(mockResponse);
   });
 });
